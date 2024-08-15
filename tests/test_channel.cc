803e3f63b80f61e40097c3462bc0794de4682aee
lane <xiurt@outlook.com>
2024-08-15 14:59:00 +0800

    尝试增加select

--- tests/test_channel.cc
+++ tests/test_channel.cc
@@ -1,35 +1,36 @@
 
 #include <iostream>
+
 #include "base/channel.h"
 #include "base/iomanager.h"
-lane::channel<int> ch(1);
-static int g_int = 0;
+lane::channel<int>       ch(1);
+static int               g_int = 0;
 static lane::Logger::ptr g_logger = LANE_LOG_NAME("system");
-void producer(){
-    while(true) {
+void                     producer() {
+    while (true) {
         sleep(1);
-        LANE_LOG_INFO(g_logger) << "in g_int="<< g_int;
+        LANE_LOG_INFO(g_logger) << "in g_int=" << g_int;
         ch.input(g_int++);
     }
 }
 
 void comsumer() {
-    while(true) {
+    while (true) {
         sleep(3);
         int tmp = ch.output();
-        LANE_LOG_INFO(g_logger) << "out g_int="<<tmp;
+        LANE_LOG_INFO(g_logger) << "out g_int=" << tmp;
     }
 }
 
 void waitSignl() {
     sleep(1);
     ch.output();
-    LANE_LOG_INFO(g_logger)<< "quit";
+    LANE_LOG_INFO(g_logger) << "quit left size=" << ch.size();
 }
 void releaseSignl() {
     sleep(3);
-    LANE_LOG_INFO(g_logger)<< "start release";
-    for(int i = 0;i < 3; i++) {
+    LANE_LOG_INFO(g_logger) << "start release";
+    for (int i = 0; i < 3; i++) {
         ch.input(true);
     }
 }
