55a95da4674420e4446f0cb822a768224ae8922c
lane <xiurt@outlook.com>
2024-08-16 17:14:55 +0800

    增加断言开关，方便压测

--- src/base/macro.h
+++ src/base/macro.h
@@ -12,26 +12,35 @@
 
 #include <assert.h>
 #include <string.h>
+
 #include "base/log.h"
 #include "base/util.h"
 
 /// 断言宏封装
-#define LANE_ASSERT(x)                                  \
-    if (!(x)) {                                         \
-        LANE_LOG_ERROR(LANE_LOG_ROOT())                 \
-            << "ASSERTION: " #x << "\nbacktrace:\n"     \
-            << lane::BacktraceToString(100, 2, "    "); \
-        assert(x);                                      \
+#define LANE_ASSERT_ON false
+
+#define LANE_ASSERT(x)                                      \
+    if (LANE_ASSERT_ON) {                                   \
+        if (!(x)) {                                         \
+            LANE_LOG_ERROR(LANE_LOG_ROOT())                 \
+                << "ASSERTION: " #x << "\nbacktrace:\n"     \
+                << lane::BacktraceToString(100, 2, "    "); \
+            assert(x);                                      \
+        }                                                   \
     }
 
+
 /// 断言宏封装
-#define LANE_ASSERT2(x, w)                              \
-    if (!(x)) {                                         \
-        LANE_LOG_ERROR(LANE_LOG_ROOT())                 \
-            << "ASSERTION: " #x << "\n"                 \
-            << w << "\nbacktrace:\n"                    \
-            << lane::BacktraceToString(100, 2, "    "); \
-        assert(x);                                      \
+
+#define LANE_ASSERT2(x, w)                                  \
+    if (LANE_ASSERT_ON) {                                   \
+        if (!(x)) {                                         \
+            LANE_LOG_ERROR(LANE_LOG_ROOT())                 \
+                << "ASSERTION: " #x << "\n"                 \
+                << w << "\nbacktrace:\n"                    \
+                << lane::BacktraceToString(100, 2, "    "); \
+            assert(x);                                      \
+        }                                                   \
     }
 
 #endif
