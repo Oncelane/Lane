3f9f060a19f5c51d3ea05d70d3fe3501dbebecb8
lane-home <Xiurt@outlook.com>
2024-08-17 13:29:41 +0800

    调整为O3编译

--- tests/test_httpserver.cc
+++ tests/test_httpserver.cc
@@ -6,40 +6,39 @@ static lane::Logger::ptr g_logger = LANE_LOG_ROOT();
 #define XX(...) #__VA_ARGS__
 
 lane::IOManager::ptr worker;
-void run() {
+void                 run() {
     g_logger->setLevel(lane::LogLevel::INFO);
     // lane::http::HttpServer::ptr server(new lane::http::HttpServer(true,
     // worker.get(), lane::IOManager::GetThis()));
     lane::http::HttpServer::ptr server(new lane::http::HttpServer(true));
     lane::Address::ptr addr = lane::Address::LookupAnyIPAddress("0.0.0.0:8020");
     while (!server->bind(addr)) {
-        sleep(2);
     }
     auto sd = server->getServletDispatch();
     sd->addServlet("/sylar/xx",
-                   [](lane::http::HttpRequest::ptr req,
+                   [](lane::http::HttpRequest::ptr  req,
                       lane::http::HttpResponse::ptr rsp,
-                      lane::http::HttpSession::ptr session) {
+                      lane::http::HttpSession::ptr  session) {
                        rsp->setBody(req->toString());
                        return 0;
                    });
 
     sd->addGlobServlet("/sylar/*",
-                       [](lane::http::HttpRequest::ptr req,
+                       [](lane::http::HttpRequest::ptr  req,
                           lane::http::HttpResponse::ptr rsp,
-                          lane::http::HttpSession::ptr session) {
+                          lane::http::HttpSession::ptr  session) {
                            rsp->setBody("Glob:\r\n" + req->toString());
                            return 0;
                        });
 
     sd->addGlobServlet(
         "/sylar/*",
-        [](lane::http::HttpRequest::ptr req,
+        [](lane::http::HttpRequest::ptr  req,
            lane::http::HttpResponse::ptr rsp,
-           lane::http::HttpSession::ptr session) {
+           lane::http::HttpSession::ptr  session) {
             rsp->setBody(XX(<html><head><title> 404 Not Found</ title></ head>
                                 <body><center><h1> 404 Not Found</ h1></ center>
-                                <hr><center> nginx /
+                                <hr><center>               nginx /
                                 1.16.0 <
                             / center > </ body></ html>));
             return 0;
